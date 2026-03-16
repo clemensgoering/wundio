1f0cac1a208026a1fa548bfd02d0b49a835bd566
Clemens Huck <clemens.goe@gmx.de>
2026-03-16 21:24:13 +0100

    Lint: Correction

--- core/services/hardware.py
+++ core/services/hardware.py
@@ -10,7 +10,7 @@ Tier mapping:
 
 import re
 import logging
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Optional
 
 logger = logging.getLogger(__name__)
